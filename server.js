eab7f023a472a59afdaa5fa73f0360ee7d3fb99c
Diana <dianabistrik@gamil.com>
2025-12-24 16:57:39 +0200

    Devided into client folder and server files

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 // Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname)));
+
 
 // Start server
 app.listen(PORT, () => {
